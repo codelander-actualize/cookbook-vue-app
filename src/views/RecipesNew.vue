cdcfc82388382710baecad0b774ada9a348e12ed
dani <dani.zaghian@gmail.com>
2019-02-21 10:44:26 -0800

    adds forms and error handling to recipes new

--- src/views/RecipesNew.vue
+++ src/views/RecipesNew.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="recipes-new">
     <h1>New Recipe</h1>
-    <div>
-      Title: <input type="text" v-model="newRecipeTitle"><br>
-      Ingredients: <input type="text" v-model="newRecipeIngredients"><br>
-      Directions: <input type="text" v-model="newRecipeDirections"><br>
-      Prep Time: <input type="text" v-model="newRecipePrepTime"><br>
-      Image Url: <input type="text" v-model="newRecipeImageUrl"><br>
 
-      <button v-on:click="createRecipe()">Create</button>
-    </div>
+		<!-- <img v-if="status" v-bind:src="'https://http.cat/' + status" alt=""> -->
+    <ul>
+    	<li v-for="error in errors">{{ error }}</li>
+    </ul>
+
+    <form v-on:submit.prevent="submit()">
+    	<div>
+    	  Title: <input type="text" v-model="newRecipeTitle">
+    	</div>
+    	<div>
+    		Ingredients: <input type="text" v-model="newRecipeIngredients">
+    	</div>
+    	<div>
+    		Directions: <input type="text" v-model="newRecipeDirections">
+    	</div>
+    	<div>
+    		Prep Time: <input type="text" v-model="newRecipePrepTime">
+    	</div>
+    	<div>
+    		Image Url: <input type="text" v-model="newRecipeImageUrl">
+    	</div>
+			<div>
+				<input type="submit" value="Create">
+			</div>
+    </form>
+
   </div>
 </template>
 
@@ -23,11 +41,13 @@ export default {
       newRecipeIngredients: "",
       newRecipeDirections: "",
       newRecipePrepTime: "",
-      newRecipeImageUrl: ""
+      newRecipeImageUrl: "",
+      errors: [],
+      // status: ""
     };
   },
   methods: {
-    createRecipe: function() {
+    submit: function() {
       var recipeParams = {
         title: this.newRecipeTitle,
         ingredients: this.newRecipeIngredients,
@@ -39,6 +59,9 @@ export default {
       axios.post("/api/recipes", recipeParams).then(response => {
         console.log("Success!", response.data);
         this.$router.push("/");
+      }).catch(error => {
+      	this.errors = error.response.data.errors;
+      	// this.status = error.response.status;
       });
     }
   }
