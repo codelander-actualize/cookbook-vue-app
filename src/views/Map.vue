75ed09d4d355e0f0ed61b3c88682980d5e47947e
dani <dani.zaghian@gmail.com>
2019-02-28 10:43:01 -0800

    adds markers and popups

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -7,7 +7,12 @@
 </template>
 
 <style>
-  #map { width: 700px; }
+  #map { 
+    width: 700px; 
+  }
+  .mapboxgl-popup {
+    max-width: 200px;
+  }
 </style>
 
 <script>
@@ -19,12 +24,26 @@ export default {
   },
   mounted: function() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZHphZ2hpYW4iLCJhIjoiY2pzbnF0NmV0MGY2czQzbXBpMjcwMzRmNiJ9.Jei4-17Vu7hJSerisjPCEg';
+
+    var marinis = [-122, 37]
+    //create the map
     var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-    center: [-122.4, 37.7], // starting position [lng, lat]
+    center: marinis, // starting position [lng, lat]
     zoom: 10 // starting zoom
     });
+
+    // create the popup
+    var popup = new mapboxgl.Popup({ offset: 25 })
+      .setText('This is my favorite ice cream in Santa Cruz');
+
+    // create the marker
+    var marker = new mapboxgl.Marker()
+      .setLngLat(marinis)
+      .setPopup(popup) // sets a popup on this marker
+      .addTo(map);
+
   },
   methods: {}
 };
